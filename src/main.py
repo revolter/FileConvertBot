revolter/FileConvertBot

commit a5a73e0a92dd5aad2f0d1569867ce1dfa8748fbc

Fixed crash for some files

src/main.py
@@ -179,7 +179,7 @@ def message_file_handler(update: Update, context: CallbackContext):
 
         if probe:
             for stream in probe['streams']:
-                codec_name = stream['codec_name']
+                codec_name = stream.get('codec_name')
 
                 invalid_format = codec_name
 

commit 1b36124518b3bad1a37226082ba43bc027019784

Fixed incorrect invalid file format being reported
(introduced by a5a73e0a)

src/main.py
@@ -185,7 +185,8 @@ def message_file_handler(update: Update, context: CallbackContext):
             for stream in probe['streams']:
                 codec_name = stream.get('codec_name')
 
-                invalid_format = codec_name
+                if codec_name is not None:
+                    invalid_format = codec_name
 
                 if codec_name == 'mp3':
                     output_type = OutputType.AUDIO
@@ -360,7 +361,8 @@ def message_video_handler(update: Update, context: CallbackContext):
             for stream in probe['streams']:
                 codec_name = stream.get('codec_name')
 
-                invalid_format = codec_name
+                if codec_name is not None:
+                    invalid_format = codec_name
 
                 if codec_name in VIDEO_CODEC_NAMES:
                     output_type = OutputType.VIDEO_NOTE
@@ -538,7 +540,8 @@ def message_answer_handler(update: Update, context: CallbackContext):
             for stream in probe['streams']:
                 codec_name = stream.get('codec_name')
 
-                invalid_format = codec_name
+                if codec_name is not None:
+                    invalid_format = codec_name
 
                 if codec_name in VIDEO_CODEC_NAMES:
                     output_type = OutputType.VIDEO_NOTE