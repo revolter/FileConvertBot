52a91af546425ceb3c4e9376e96accbea341105d
Iulian Onofrei <iulian.onofrei@yahoo.com>
2020-07-29 21:08:26 +0300

    Style: Fixed code comment style

--- src/constants.py
+++ src/constants.py
@@ -4,7 +4,7 @@ import datetime
 
 GOOGLE_HEADERS = {'User-Agent': 'FileConvertBot'}
 
-#: See also: https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+# See also: https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
 GOOGLE_ANALYTICS_BASE_URL = 'https://www.google-analytics.com/collect?v=1&t=event&tid={}&cid={}&ec={}&ea={}'
 
 LOGS_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
